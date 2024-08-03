1af6a98b7e795010a8c031daffaeca9021f9a1e1
sukaf <dcghjas@gmail.com>
2024-08-03 22:34:56 +0300

    fin

--- education/celery.py
+++ education/celery.py
@@ -14,6 +14,4 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Автоматически искать задачи в приложениях Django
 app.autodiscover_tasks()
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+
