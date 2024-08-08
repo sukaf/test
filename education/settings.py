5cc6bbdda988b6a1daf3e6ec7f4fca0338876103
sukaf <dcghjas@gmail.com>
2024-08-08 05:48:14 +0300

    fin

--- education/settings.py
+++ education/settings.py
@@ -167,11 +167,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (BASE_DIR / 'static')
+STATIC_ROOT = (BASE_DIR / '/education/static/')
 
 STATICFILES_DIRS = [BASE_DIR / 'templates/src']
 
-MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_ROOT = (BASE_DIR / '/education/media/')
 MEDIA_URL = '/media/'
 
 
